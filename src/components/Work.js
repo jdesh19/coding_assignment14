ef68c73f26df5c677554609e4c1c4d160fd27864
jdesh19 <jarondesharnais@gmail.com>
2024-08-05 11:20:18 -0500

    hook Test

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,44 +1,34 @@
-import React from 'react';
-import { work } from '../data';
+import React from "react";
+import { work } from "../data";
 
 export default function Work() {
-    return(
-        <section id="work">
+  return (
+    <section id="work">
       <div>
         <div>
-          <h1 className="sm:text-2xl text-2xl font-medium title-font text-white"> My Work </h1>
-          <p>
-            Here are some of the projects I have worked on.
-          </p>
+          <h1 className="sm:text-2xl text-2xl font-medium title-font text-white">
+            {" "}
+            My Work{" "}
+          </h1>
+          <p>Here are some of the projects I have worked on.</p>
         </div>
         <div>
           {work.map((project, index) => (
             <div key={index}>
               <div>
-                <img
-                  src={project.image}
-                  alt="project"
-                />
+                <img src={project.image} alt="project" />
                 <div>
-                  <h2>
-                    {project.title}
-                  </h2>
+                  <h2>{project.title}</h2>
                   <p>{project.description}</p>
                   <div>
-                    <a
-                      href={project.link}
-                      target="_blank"
-                    >
+                    <a href={project.link} target="_blank" rel="noreferrer">
                       View Project
-                      
                     </a>
                   </div>
                   <h3>Technologies Used:</h3>
                   <ul>
                     {project.tech_list.map((tech, index) => (
-                      <li key={index}>
-                        {tech}
-                      </li>
+                      <li key={index}>{tech}</li>
                     ))}
                   </ul>
                 </div>
@@ -47,7 +37,7 @@ export default function Work() {
           ))}
         </div>
       </div>
-      <br/>
+      <br />
     </section>
-    );
+  );
 }
